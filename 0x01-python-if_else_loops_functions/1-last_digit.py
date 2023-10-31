78c023aa99463a7721a9ac4c4b1d1086bee3610b
Soledadkesi <soledadkesi1995@gmail.com>
2023-10-31 10:56:29 -0700

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 import random
-
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
 
-print(f'The string Last digit of {number} is {last_digit}', end=" ")
-if last_digit > 5:
-    print(f"{number} and is greater than 5")
+if number < 0:
+    rem = number % (-10)
+else:
+    rem = number % 10
 
-elif last_digit == 0:
-    print(f"{number} and is 0")
+if rem > 5:
+    print(f"Last digit of {number} is {rem} and is greater than 5")
+
+elif rem == 0:
+    print(f"Last digit of {number} is {rem} and is 0")
 
 else:
-    print(f"{number} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {rem} and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,13 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
- * struct listint_s - the singly linked list
- * @n: the integer
- * @next: the points to the next node
- * Description: the singly linked list node structure
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  *
  */
 typedef struct listint_s
@@ -16,10 +18,9 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void free_listint(listint_t *head);
-listint_t *insert_node(listint_t **head, int number);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
-
+void free_listint(listint_t *head);
+listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
