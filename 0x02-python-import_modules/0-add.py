bc8498501402a0f875f6b729ddfd5a2aa07b647e
Soledadkesi <soledadkesi1995@gmail.com>
2023-11-02 05:14:48 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    from add_0 import add
 
     a = 1
     b = 2
 
-    result = a + b
-    print(f'{a} + {b} = {result}')
+    print("{} + {} = {}".format(a, b, add(a, b)))
